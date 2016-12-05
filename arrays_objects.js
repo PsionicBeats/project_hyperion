636adc38e344213bd4f2198abd270116767d6cbb
Alex Aguinaldo <psionicbones@gmail.com>
2016-12-05 08:40:28 -1000

    update

---
+++ arrays_objects.js
@@ -0,0 +1,21 @@
+/* 
+	Alex Aguinaldo 
+ 	Arrays and Objects 
+ 	Dec. 5, 2016
+*/
+
+// variables
+// ARRAYS
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+var names = ["Senior Tan", "Zakattak", "Yolo Baggins", "Ohai", "Jeff"];
+var colors = ["black", "ron burgandy", "highlighter"];
+var adj = ["smart", "special case", "yummy", "tiny whiny", "ferocious"];
+var actions = ["scrap", "kobe beef", "da kine", "li 'dat", "pau hana"];
+
+// OBJECTS
+var donut = {
+	topping : ["sprinkles", "choco frosting", "o'hare air"],
+	filling : "Jelly",
+	shape : "Square",
+	count : 12
+}
